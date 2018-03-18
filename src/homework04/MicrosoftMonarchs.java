bd3710d101fe9475608bb7eb628e76f172cd1474
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-03-18 18:30:36 -0400

    Moving file into package

---
+++ src/homework04/MicrosoftMonarchs.java
@@ -0,0 +1,12 @@
+package homework04;
+
+public class MicrosoftMonarchs {
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Microsoft Monarchs!");
+        System.out.println("------------------------------");
+
+    }
+
+}

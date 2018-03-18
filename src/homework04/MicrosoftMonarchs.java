9974027ce34723c498f59837d9df4cd8c81d909b
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-03-18 18:43:36 -0400

    Added text file to read from and temporary try-catch block to read from it.

--- src/homework04/MicrosoftMonarchs.java
+++ src/homework04/MicrosoftMonarchs.java
@@ -1,11 +1,33 @@
 package homework04;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MicrosoftMonarchs {
+
+    private static final String filePath = "MicrosoftStockData.txt";
 
     public static void main(String[] args){
 
+        Scanner fileIn = null;
+        String line;
+
         System.out.println("Welcome to Microsoft Monarchs!");
         System.out.println("------------------------------");
+
+        try
+        {
+            fileIn = new Scanner(new FileInputStream(filePath));
+            while (fileIn.hasNextLine()){
+                line = fileIn.nextLine();
+                System.out.println(line);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Unable to find or open the file.");
+        }
 
     }
 
